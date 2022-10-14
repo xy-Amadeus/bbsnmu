3279e428cd00ff810ac3db75055c5805becb9f78
xy-Amadeus <595302874@qq.com>
2022-10-14 11:57:05 +0800

    init repo

---
+++ src/main/java/Bbsnmu/BbsnmuApplication.java
@@ -0,0 +1,13 @@
+package Bbsnmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsnmuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsnmuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bbsnmu/BbsnmuApplicationTests.java
@@ -0,0 +1,13 @@
+package bbsnmu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsnmuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
